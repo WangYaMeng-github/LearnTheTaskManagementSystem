1cedc1ed08ced1bb7c3abd98c4ba3d2f3d317ede
WangYaMeng-github <tt_wangyameng@163.com>
2021-06-07 16:40:36 +0800

    基础搭建+添加logo+优化日志

--- src/main/java/com/laborer/controller/TestController.java
+++ src/main/java/com/laborer/controller/TestController.java
@@ -1,9 +1,22 @@
 package com.laborer.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Auther: Wangym
  * @Date: 2021/6/7 11:32
  * @Description:
  */
+@Controller
+@RequestMapping("/index")
 public class TestController {
+
+    @RequestMapping("")
+    public String index(Model model){
+        model.addAttribute("name","王亚萌");
+        return "index";
+    }
+
 }
